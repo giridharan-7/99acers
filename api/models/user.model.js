4cfd24aeab1a74303eddbead3ea9742744786972
giridharan-7 <giridharanias7@gmail.com>
2024-03-17 05:15:29 +0530

    create user model

---
+++ api/models/user.model.js
@@ -0,0 +1,25 @@
+import { Mongoose } from "mongoose";
+
+//just creating a schema(rules) for the user
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+}, { timestamps: true });
+
+// Timestamp Tell mongodb to record the two info one is time of creation of user and the time of the pdate of the user
+
+const User = mongoose.model('user', userSchema);
+
+export default User;
