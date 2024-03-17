9428d2429f5ae7aebad576e9004de5d803c7802a
giridharan-7 <giridharanias7@gmail.com>
2024-03-17 19:27:14 +0530

    complete signup page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,21 +1,68 @@
-import {Link} from 'react-router-dom'
+import { useState } from 'react'
+import {Link, useNavigate} from 'react-router-dom'
 
 export default function SignUp() {
-  return (
-    <div className='p-3 max-w-lg mx-auto'>
-        <h1 className='text-3xl text-center font-semibold my-7'>SignUp</h1>
-        <form className='flex flex-col gap-4'>
-            <input type="text" placeholder='username' className='border p-3 rounded-lg' id='username' />
-            <input type="email" placeholder='email' className='border p-3 rounded-lg' id='email' />
-            <input type="password" placeholder='password' className='border p-3 rounded-lg' id='password' />
-            <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-8-'>SignUp</button>
-        </form>
-        <div className='flex'>
-            <p>Have an account?</p>
-            <Link to={"/signin"}>
-                <span className='text-blue-700'>Sign-In</span>
-            </Link>
+    const [formData, setFormData] = useState({})
+    const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(false)
+    const navigate = useNavigate()
+
+    const handleChange = (e) => {
+        setFormData(
+            {
+                ...formData,
+                [e.target.id] : e.target.value,
+            }
+        )
+    }
+    const handleSubmit = async(e) => {
+        // It prevents refreshing the page when we click the submit button
+        e.preventDefault();
+        try{
+            setLoading(true)
+            // use the fetch method to req the api route localhost:3000/api/auth/signup
+            const res = await fetch('/api/auth/signup', 
+            {
+                method:'POST',
+                headers:{
+                    'Content-Type':'application/json'
+                    // Indicating that the request body is json formated
+                },
+                body: JSON.stringify(formData),
+                // Converts the formData to a json string
+            });
+            const data = await res.json();
+            if(data.success === false){
+                setError(data.message)
+                setLoading(false)
+                return;
+            }
+            setLoading(false)
+            setError(null)
+            navigate('/signin')
+        }catch(error){
+            setLoading(false)
+            setError(error.message)
+        }
+        
+    }
+    console.log(formData)
+    return (
+        <div className='p-3 max-w-lg mx-auto'>
+            <h1 className='text-3xl text-center font-semibold my-7'>SignUp</h1>
+            <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+                <input type="text" placeholder='username' className='border p-3 rounded-lg' id='username' onChange={handleChange}/>
+                <input type="email" placeholder='email' className='border p-3 rounded-lg' id='email' onChange={handleChange}/>
+                <input type="password" placeholder='password' className='border p-3 rounded-lg' id='password' onChange={handleChange}/>
+                <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-8-'>{loading ? 'Loading...' : 'SignUp' }</button>
+            </form>
+            <div className='flex'>
+                <p>Have an account?</p>
+                <Link to={"/signin"}>
+                    <span className='text-blue-700'>Sign-In</span>
+                </Link>
+            </div>
+            {error && <p className='text-red-500'>{error}</p>}
         </div>
-    </div>
-  )
+    )
 }
--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server:{
+    proxy: {
+      // Each time you say /api [client/src/pages/SignUp.jsx] you need to append the target to the front 
+      '/api': {
+        target: 'http://localhost:3000',
+        secure: false,
+      },
+    },
+  },
   plugins: [react()],
 })
